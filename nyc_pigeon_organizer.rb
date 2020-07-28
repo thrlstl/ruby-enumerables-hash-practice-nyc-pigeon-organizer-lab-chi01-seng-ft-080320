8e254d0e9fb7f4e209ad72676e2e7051d9ce0b2c
Matthew Steele <matthewsteeleonline@gmail.com>
2020-07-28 01:43:15 +0000

    Automatically backed up by Learn

--- nyc_pigeon_organizer.rb
+++ nyc_pigeon_organizer.rb
@@ -8,8 +8,7 @@ def nyc_pigeon_organizer(data)
   
 end
 
-
-  def nyc_pigeon_organizer(data)
+def nyc_pigeon_organizer(data)
   new_hash = {}
   data.each do |key, value|
     value.each do |new_value, names|
